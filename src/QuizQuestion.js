d1ff4f6d1169dd2ab7c9dbd39718854f2c390cc0
Camilo Crespo <crezam@live.com>
2018-07-22 01:11:47 -0400

    Conditional error paragraph

--- src/QuizQuestion.js
+++ src/QuizQuestion.js
@@ -2,11 +2,20 @@ import React, {Component} from 'react'
 import QuizQuestionButton from './QuizQuestionButton.js'
 
 class QuizQuestion extends Component {
+    constructor(props) {
+        super(props)
+        this.state = {incorrectAnswer: false}
+    }
+
     handleClick(buttonText) {
-        if(buttonText == this.props.quiz_question.answer) {
+        if (buttonText == this.props.quiz_question.answer) {
+            this.setState({incorrectAnswer: false})
             this.props.showNextQuestionHandler()
+        } else {
+            this.setState({incorrectAnswer: true})
         }
     }
+
     render() {
         return (
             <main>
@@ -16,10 +25,12 @@ class QuizQuestion extends Component {
                 <section className="buttons">
                     <ul>
                         {this.props.quiz_question.answer_options.map((answer_option, index) =>
-                            <QuizQuestionButton key={index} button_text={answer_option} clickHandler={this.handleClick.bind(this)}/>
+                            <QuizQuestionButton key={index} button_text={answer_option}
+                                                clickHandler={this.handleClick.bind(this)}/>
                         )}
                     </ul>
                 </section>
+                {this.state.incorrectAnswer ? <p className='error'>Sorry, that's not right</p> : null}
             </main>
         )
     }
