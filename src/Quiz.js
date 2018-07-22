f7f53a34ec5d04f9453817d442bbe7e8031352b5
Camilo Crespo <crezam@live.com>
2018-07-21 20:11:14 -0400

    Add click handlers and state mods

--- src/Quiz.js
+++ src/Quiz.js
@@ -10,12 +10,20 @@ class Quiz extends Component {
         this.state = {quiz_position: 1}
     }
 
+    showNextQuestion() {
+        this.setState((state) => {
+            return {quiz_position: state.quiz_position + 1}
+
+        })
+    }
+
     render() {
         const isQuizEnd = this.state.quiz_position - 1 == quizData.quiz_questions.length
         if (isQuizEnd) {
             return <QuizEnd/>
         } else {
-            return <QuizQuestion quiz_question={quizData.quiz_questions[this.state.quiz_position - 1]}/>
+            return <QuizQuestion quiz_question={quizData.quiz_questions[this.state.quiz_position - 1]}
+                                 showNextQuestionHandler={this.showNextQuestion.bind(this)}/>
         }
     }
 }
